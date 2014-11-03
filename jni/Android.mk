e7e8bb3b263bbaa94fcfc6fb8127e56f3794ca93
QEver <QEver.cn@gmail.com>
2014-11-03 11:04:22 +0800

    init

---
+++ jni/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)  
+  
+include $(CLEAR_VARS)  
+LOCAL_MODULE := inject   
+LOCAL_SRC_FILES := \
+            inject.c
+  
+LOCAL_LDLIBS += -llog  
+  
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)  
+LOCAL_MODULE := qever   
+LOCAL_SRC_FILES := \
+            qever.c
+  
+LOCAL_LDLIBS += -llog  -pthread
+  
+include $(BUILD_SHARED_LIBRARY)
